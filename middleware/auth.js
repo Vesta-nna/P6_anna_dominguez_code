30462f2764e38e2660dd2045844c71ff06d39a84
Vesta-nna <anna.dominguez.anna@gmail.com>
2021-08-14 22:36:51 +0200

    add images folder

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,8 +3,11 @@ const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
+    console.log("auth")
+    console.log("token = ", token)
     const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
     const userId = decodedToken.userId;
+    console.log("userID = ", userId)
 
     if (req.body.userId && req.body.userId !== userId) throw "Invalid user ID";
     next();
